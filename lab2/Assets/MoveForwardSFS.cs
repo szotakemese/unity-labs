a7959d96462acca12c87d434cf9e26b9f0d1e129
Emeshe Sotak <48476041+szotakemese@users.noreply.github.com>
2020-10-09 13:29:36 +0200

    Add files via upload

---
+++ lab2/Assets/MoveForwardSFS.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForwardSFS : MonoBehaviour
+{
+    public float Speed = 1f;
+    public Vector3 Direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Direction = transform.forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Direction * Speed * Time.deltaTime);
+       
+    }
+}
