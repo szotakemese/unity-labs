744d5d578a0fb5062226ecc5413792172e63f8e9
Emeshe Sotak <48476041+szotakemese@users.noreply.github.com>
2020-10-09 13:32:05 +0200

    Add files via upload

---
+++ lab2-2/Assets/PlywoodForce.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlywoodForce : MonoBehaviour
+{
+    public float thrust = 3000f;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * thrust);
+    }
+
+}
